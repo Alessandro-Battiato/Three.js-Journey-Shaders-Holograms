0a75888a78df28c242a4fbdd89b0bf9d716b4060
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-04-06 18:20:24 +0200

    feat: stripes implemented

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -1,6 +1,11 @@
+varying vec3 vPosition;
+
 void main() {
+    // Stripes
+    float stripes = mod(vPosition.y * 20.0, 1.0);
+
     // Final color
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4(stripes, stripes, stripes, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment> // RGB to sRGB
 }
--- src/shaders/holographic/vertex.glsl
+++ src/shaders/holographic/vertex.glsl
@@ -1,7 +1,12 @@
+varying vec3 vPosition;
+
 void main() {
     // Position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0); 
 
     // Final position
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
+
+    // Varyings
+    vPosition = modelPosition.xyz; // being vPosition a vec3, we extract only modelPosition's x y and z, this process is known as swizzling
 }
