0e605de4f6d7d3e1d7ba955778898eead88f4d65
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-04-07 11:04:23 +0200

    feat: hologram looking like models implemented

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -14,9 +14,14 @@ void main() {
     // Fresnel
     vec3 viewDirection = normalize(vPosition - cameraPosition); // subtract the destination (vPosition) to the 3D camera position as to get the Normal vector, and we Normalize everything as to force the length to be 1 even though the direction remains the same
     float fresnel = dot(viewDirection, normal) + 1.0;// Dot product (if Vectors are the same direction, we get 1, if they're perpendicular we get 0, if they're opposite we get -1, and if they're like in between of being perpendicular, we get 0.5) and finally the + 1.0 is needed in order to fix the range of the values from 0 to 1
+    fresnel = pow(fresnel, 2.0); // pushes the gradient outward
+
+    // Holographic
+    float holographic = stripes * fresnel;
+    holographic += fresnel * 1.25; // this makes the models more visible on the surface
 
     // Final color
-    gl_FragColor = vec4(1.0, 1.0, 1.0, fresnel); // stripes used as alpha value turns models transparent
+    gl_FragColor = vec4(1.0, 1.0, 1.0, holographic); // stripes used as alpha value turns models transparent
     #include <tonemapping_fragment>
     #include <colorspace_fragment> // RGB to sRGB
 }
